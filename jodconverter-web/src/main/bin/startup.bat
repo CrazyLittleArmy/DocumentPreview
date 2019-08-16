e7d8b4d9958a97a03a32f57ab88f42d86a24759c
shanyejun <shanyejun@servicestrong.cn>
2019-08-16 10:53:54 +0800

    modify

--- jodconverter-web/src/main/bin/startup.bat
+++ jodconverter-web/src/main/bin/startup.bat
@@ -1,7 +1,7 @@
 @echo off
-set "KKFILEVIEW_BIN_FOLDER=%cd%"
-cd "%KKFILEVIEW_BIN_FOLDER%"
-echo Using KKFILEVIEW_BIN_FOLDER %KKFILEVIEW_BIN_FOLDER%
-echo Starting kkFileView...
+set "DOCUMENTPREVIEW_BIN_FOLDER=%cd%"
+cd "%DOCUMENTPREVIEW_BIN_FOLDER%"
+echo Using KKFILEVIEW_BIN_FOLDER %DOCUMENTPREVIEW_BIN_FOLDER%
+echo Starting DocumentPreview...
 echo Please check log file for more information
-java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar kkFileView-2.1.2.jar -> ..\log\kkFileView.log
+java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar DocumentPreview-2.1.2.jar -> ..\log\kkFileView.log
--- jodconverter-web/src/main/bin/startup.sh
+++ jodconverter-web/src/main/bin/startup.sh
@@ -2,8 +2,8 @@
 DIR_HOME=("/opt/openoffice.org3" "/opt/libreoffice" "/opt/openoffice4" "/usr/lib/openoffice" "/usr/lib/libreoffice")
 FLAG=
 OFFICE_HOME=
-KKFILEVIEW_BIN_FOLDER=$(cd "$(dirname "$0")";pwd)
-export KKFILEVIEW_BIN_FOLDER=$KKFILEVIEW_BIN_FOLDER
+DOCUMENTPREVIEW_BIN_FOLDER=$(cd "$(dirname "$0")";pwd)
+export DOCUMENTPREVIEW_BIN_FOLDER=$DOCUMENTPREVIEW_BIN_FOLDER
 cd $KKFILEVIEW_BIN_FOLDER
 echo "Using KKFILEVIEW_BIN_FOLDER $KKFILEVIEW_BIN_FOLDER"
 grep 'office\.home' ../conf/application.properties | grep '!^#'
@@ -27,13 +27,8 @@ else
 fi
 echo "Starting kkFileView..."
 echo "Please check log file for more information"
-nohup /app/software/jdk1.8.0_181/bin/java -Dfile.encoding=UTF-8 -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=../conf/application.properties -jar kkFileView-2.1.2.jar > ../log/kkFileView.log 2>&1 &
-pid=`ps -ef | grep 'kkFileView-2.1.2.jar' | grep -v grep | awk '{print $2}'`
-if [ -z "${pid}" ]; then
-   echo "kkFileView-2.1.2.jar is not running"
-else
-    echo "kkFileView-2.1.2.jar is   running  lalalalla"
-fi
+nohup  java -Dfile.encoding=UTF-8 -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=../conf/application.properties -jar DocumentPreview-2.1.2.jar > ../log/DocumentPreview.log 2>&1 &
+
 
 
 
