1a7c95956fe4c3acbedf0980260da312293ede91
shanyejun <shanyejun@servicestrong.cn>
2019-08-16 14:31:27 +0800

    modify

--- jodconverter-web/src/main/bin/startup.bat
+++ jodconverter-web/src/main/bin/startup.bat
@@ -4,4 +4,4 @@ cd "%DOCUMENTPREVIEW_BIN_FOLDER%"
 echo Using KKFILEVIEW_BIN_FOLDER %DOCUMENTPREVIEW_BIN_FOLDER%
 echo Starting DocumentPreview...
 echo Please check log file for more information
-java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar DocumentPreview-2.1.2.jar -> ..\log\kkFileView.log
+java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar DocumentPreview-2.1.2.jar -> ..\log\DocumentPreview.log
